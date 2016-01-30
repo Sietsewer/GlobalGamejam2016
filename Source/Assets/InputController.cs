fd92b914326de046f9785ab21fd53384d7bbfec5
joheeen <johanvandermeulen_1@hotmail.com>
2016-01-30 09:36:13 +0100

    Jump controls + Collisions

--- Source/Assets/InputController.cs
+++ Source/Assets/InputController.cs
@@ -4,6 +4,12 @@ using System.Collections;
 namespace InputManager {
 	public class InputController : MonoBehaviour {
 
+		void Start() {
+			foreach(string name in Input.GetJoystickNames() ) {
+				Debug.Log( name );
+			}
+		}
+
 		public float GetXAxis(int player) {
 			return Input.GetAxis("Horizontal_" + player.ToString() );
 		}
@@ -11,6 +17,9 @@ namespace InputManager {
 		public float GetYAxis(int player) {
 			return Input.GetAxis("Vertical_" + player.ToString() );
 		}
-			
+
+		public bool JumpButtonPressed(int player) {
+			return Input.GetButtonDown("Jump_" + player.ToString());
+		}
 	}
 }
--- Source/Assets/PlayerController.cs
+++ Source/Assets/PlayerController.cs
@@ -8,17 +8,45 @@ namespace Player {
 
 		public int player = 1;
 		public float speed = 1.0f;
+		public float jumpForce = 10f;
+
+		private bool _jumping = false;
+		private Rigidbody _rigidbody;
 
 		// Use this for initialization
 		void Start () {
-			
+			_rigidbody = GetComponent<Rigidbody>();
 		}
 		
 		// Update is called once per frame
 		void Update () {
 		
+			//player movement
 			transform.Translate( GetXAxis(player) * Time.deltaTime * speed, 0, GetYAxis(player) * Time.deltaTime * speed);
 
+			//player jumping
+			if (JumpButtonPressed(player)) {
+				if (!_jumping) {
+					Debug.Log("Player " + player.ToString() + " jump");
+					Jump();
+				}
+			}
+
+		}
+
+		void Jump() {
+			_jumping = true;
+
+			_rigidbody.AddForce(Vector3.up * jumpForce);
+		}
+
+		//Player in collision with floor reset jump to false
+		void OnTriggerEnter(Collider other) {
+			Debug.Log(other.name + " collisioned with " + gameObject.name);
+
+			if (other.tag == "floor") {
+				_jumping = false;
+			}
 		}
 	}
 }
