7e284c25ac09e14fcac7e0968258d493bbe8e861
Sietse van der Werf <sietswer@gmail.com>
2016-01-30 02:18:40 +0100

    TileGrid basics

---
+++ Source/Assets/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LevelGrid {
+	[System.Serializable]
+	public class Tile : MonoBehaviour {
+		/*
+		 * NW    NE
+		 * 	  []
+		 * SW    SE
+		 */
+
+		public Tile NorthEast;
+		public Tile SouthEast;
+		public Tile NorthWest;
+		public Tile SouthWest;
+
+		public int indexX = int.MaxValue;
+		public int indexY = int.MaxValue;
+	}
+}
