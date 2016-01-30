ab65f96e027a95797a4dcbfa8d73fb9f2bf41576
joheeen <johanvandermeulen_1@hotmail.com>
2016-01-30 02:43:09 +0100

    Basic Joystick controls 1 + 2

---
+++ Source/Assets/InputController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace InputManager {
+	public class InputController : MonoBehaviour {
+
+		public float GetXAxis(int player) {
+			return Input.GetAxis("Horizontal_" + player.ToString() );
+		}
+
+		public float GetYAxis(int player) {
+			return Input.GetAxis("Vertical_" + player.ToString() );
+		}
+			
+	}
+}
---
+++ Source/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Player;
+using InputManager;
+
+namespace Player {
+	public class PlayerController : InputController {
+
+		public int player = 1;
+		public float speed = 1.0f;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+			transform.Translate( GetXAxis(player) * Time.deltaTime * speed, 0, GetYAxis(player) * Time.deltaTime * speed);
+
+		}
+	}
+}
