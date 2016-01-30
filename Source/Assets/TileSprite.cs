1b46ddd0100e9aaf11bbba0472439ab0553130ce
Sietse van der Werf <sietswer@gmail.com>
2016-01-30 16:34:20 +0100

    Lighting_start

---
+++ Source/Assets/TileSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LevelGrid{
+	public class TileSprite : MonoBehaviour {
+		public Sprite sprite;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
