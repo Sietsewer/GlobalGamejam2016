fb1ad2eeac7d42a31aac5a3873c0afbc9b94ae0b
Sietse van der Werf <sietswer@gmail.com>
2016-01-31 12:04:36 +0100

    Added stun spell

---
+++ Source/Assets/StunEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Player;
+using System.Collections.Generic;
+
+public class StunEffect : MonoBehaviour {
+	public PlayerController player;
+
+	public List<PlayerController> stunnedPlayers = new List<PlayerController> ();
+
+	private const string shamanTag = "shaman";
+
+	void OnTriggerEnter(Collider other){
+		player = other.GetComponentInChildren<PlayerController> ();
+		if (player != null && other.tag != shamanTag) {
+			player.stunned = true;
+			stunnedPlayers.Add (player);
+		}
+	}
+
+	void OnDisable (){
+		clearStuns ();
+	}
+	void OnDestroy(){
+		clearStuns ();
+	}
+
+	private void clearStuns(){
+		foreach (PlayerController pc in stunnedPlayers) {
+			pc.stunned = false;
+		}
+		stunnedPlayers.Clear();
+	}
+}
