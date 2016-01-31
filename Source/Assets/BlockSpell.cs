5af46782cf3e6b0ab033a8f5ea570e13f641685c
Sietse van der Werf <sietswer@gmail.com>
2016-01-31 13:08:48 +0100

    minor tweaks

--- Source/Assets/BlockSpell.cs
+++ Source/Assets/BlockSpell.cs
@@ -8,14 +8,19 @@ public class BlockSpell : MonoBehaviour {
 	public float duration = 3.0f;
 	public float lastCooldownStart = 0.0f;
 
+	public float minRange = 0.3f;
+
 	public void tryCast(){
 		if(Time.time - lastCooldownStart > coolDown){
 			lastCooldownStart = Time.time;
-			closestDoor ().doorBlocker.blockDoor (duration);
+			Door d = closestDoor (minRange);
+			if (d != null) {
+				d.doorBlocker.blockDoor (duration);
+			}
 		}
 	}
 
-	private Door closestDoor(){
+	private Door closestDoor(float minDist){
 		float closestRange = float.MaxValue;
 		Door closestDoor = null;
 		Vector3 posNow = transform.position;
